82d9891cbde0612f903e90a6350cfb10910c4a6f
jairoperdomo <jairoperdomo09@gmail.com>
2021-08-01 00:08:44 -0600

    Inicio de proyecto

---
+++ src/main/java/t1center/t1app/T1AppApplication.java
@@ -0,0 +1,13 @@
+package t1center.t1app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1AppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/t1center/t1app/T1AppApplicationTests.java
@@ -0,0 +1,13 @@
+package t1center.t1app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
